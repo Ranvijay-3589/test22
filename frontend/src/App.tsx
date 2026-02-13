eaed76b34084c82bb931f3d99b03f100e20c7ff0
Developer <devloper@ideasand.com>
2026-02-13 12:42:54 +0100

    [Claude] [CODE MODE] Make direct code changes. Show complete file contents with changes. create test2 page pr...

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
+import Test2 from './pages/Test2'
+
 function App() {
-  return <div>hello word</div>
+  return (
+    <Routes>
+      <Route path="/test2" element={<Test2 />} />
+    </Routes>
+  )
 }
 
 export default App
